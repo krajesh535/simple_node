7d508792ff9103d73d5615be884b5364c0bff19c
Rajeshwar Kottala <rajeshwar.kottala@rackspace.com>
2018-08-08 12:20:06 -0500

    updated server js file port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const port = 3000 | process.env.PORT
+const port = process.env.PORT || 3000
 
-app = express();
+var app = express();
 
 app.get('/', (request, response) => {
     response.send({
